086ab75ff641804c4340f3dfec962a13e03c93b1
gmetsis@optechain.com <gmetsis@optechain.com>
2021-12-14 14:30:59 +0200

    getting-started

--- src/test/java/gr/kariera/codingschool/mindthecode/MindTheCodeApplicationTests.java
+++ src/test/java/gr/kariera/codingschool/mindthecode/MindTheCodeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo2ApplicationTests {
+class MindTheCodeApplicationTests {
 
 	@Test
 	void contextLoads() {
